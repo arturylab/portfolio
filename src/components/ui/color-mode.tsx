418e6a1f95eab22ac90f610dab3ff36e5a4ff2e8
arturylab <arturylab@gmail.com>
2025-08-27 20:04:12 -0700

    fix: replace empty interfaces with types to satisfy ESLint

--- src/components/ui/color-mode.tsx
+++ src/components/ui/color-mode.tsx
@@ -7,9 +7,7 @@ import type { ThemeProviderProps } from "next-themes"
 import * as React from "react"
 import { LuMoon, LuSun } from "react-icons/lu"
 
-export interface ColorModeProviderProps extends ThemeProviderProps {}
-
-export function ColorModeProvider(props: ColorModeProviderProps) {
+export function ColorModeProvider(props: ThemeProviderProps) {
   return (
     <ThemeProvider attribute="class" disableTransitionOnChange {...props} />
   )
@@ -17,7 +15,7 @@ export function ColorModeProvider(props: ColorModeProviderProps) {
 
 export type ColorMode = "light" | "dark"
 
-export interface UseColorModeReturn {
+export type UseColorModeReturn = {
   colorMode: ColorMode
   setColorMode: (colorMode: ColorMode) => void
   toggleColorMode: () => void
@@ -46,7 +44,7 @@ export function ColorModeIcon() {
   return colorMode === "dark" ? <LuMoon /> : <LuSun />
 }
 
-interface ColorModeButtonProps extends Omit<IconButtonProps, "aria-label"> {}
+type ColorModeButtonProps = Omit<IconButtonProps, "aria-label"> & {}
 
 export const ColorModeButton = React.forwardRef<
   HTMLButtonElement,
